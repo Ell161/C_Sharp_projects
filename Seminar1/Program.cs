83cbbf347115084725ebd5294bed75243a7a3b18
Ell161 <ev.chepiga.it@yandex.ru>
2022-12-19 18:15:47 +0300

    Seminar 1 homework

---
+++ Seminar1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+if (number_1 > number_2)
+    Console.WriteLine("Наибольшее из чисел первое " + number_1);
+else if (number_1 < number_2)
+    Console.WriteLine("Наибольшее из чисел второе " + number_2);
+else
+    Console.WriteLine("Числа равны");
+
---
+++ Seminar2/Program.cs
@@ -0,0 +1,13 @@
+﻿List<int> numbers = new List<int>(3);
+for (int i = 1; i < 4; i++)
+{
+    Console.Write($"Введите {i} число: ");
+    numbers.Add(Convert.ToInt32(Console.ReadLine()));
+}
+int max = numbers[0];
+for (int i = 1; i < 3; i++)
+{
+    if (numbers[i] > max) max = numbers[i];
+}
+Console.WriteLine("Максимальное число " + max);
+
