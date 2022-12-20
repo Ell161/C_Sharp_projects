2fec5c1d5b84f31e03ab57847d7b29d8be5ed0c2
Ell161 <ev.chepiga.it@yandex.ru>
2022-12-20 11:22:06 +0300

    Seminar 2 homework

---
+++ Seminars_2.13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+
+List<char> digits = new List<char> (number);
+int count_digits = digits.Count;
+
+if (count_digits < 3)
+{
+    Console.WriteLine("У заданного числа нет третьей цифры");
+}
+else
+{
+    Console.WriteLine($"Третья цифра заданного числа {digits[2]}");
+}
---
+++ Seminars_2.15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Ввведите день недели цифрой: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (1 <= number & number < 6)
+{
+    Console.WriteLine("Этот день недели не выходной!");
+}
+else if (6 <= number & number <= 7)
+{
+    Console.WriteLine("Это выходной!");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
