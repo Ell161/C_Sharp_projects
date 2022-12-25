8bb445dac857b2a4109fb05cd5d54caac204dc90
Ell161 <ev.chepiga.it@yandex.ru>
2022-12-25 17:41:48 +0300

    Seminar 3 homework

---
+++ Seminar_3.19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+bool result = true;
+int count_simbols = number.Length;
+
+
+for (int i = 0; i <= (count_simbols / 2); i++)
+{
+    if (number[i] != number[count_simbols - i - 1]) 
+    {
+        result = false;
+        Console.WriteLine("Число не является палиндромом");
+        break;
+    }
+}
+
+if (result) Console.WriteLine("Число является палиндромом");
---
+++ Seminar_3.21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Dictionary<string, int> points = new Dictionary<string, int>();
+
+for (int i = 1; i < 3; i++)
+{
+    Console.Write($"Введите координату х {i}-й точки: ");
+    points.Add($"x{i}", Convert.ToInt32(Console.ReadLine()));
+    Console.Write($"Введите координату y {i}-й точки: ");
+    points.Add($"y{i}", Convert.ToInt32(Console.ReadLine()));
+    Console.Write($"Введите координату z {i}-й точки: ");
+    points.Add($"z{i}", Convert.ToInt32(Console.ReadLine()));
+}
+
+double distance = Math.Pow((Math.Pow(points["x2"] - points["x1"], 2) + Math.Pow(points["y2"] - points["y1"], 2) + Math.Pow(points["z2"] - points["z1"], 2) * 1.0), 0.5);
+Console.WriteLine($"Расстояние между точками равно {distance}");
