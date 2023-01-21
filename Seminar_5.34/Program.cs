e050986a8ca258e819766bd4d1a9f922895e3233
Ell161 <ev.chepiga.it@yandex.ru>
2023-01-21 18:44:34 +0300

    Seminar 5 homework

---
+++ Seminar_5.34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
+
+
+int[] FillArray()
+// The method creates an array of random three-digit numbers
+
+{
+    Console.WriteLine("Введите длину массива:");
+    int LenghtArray = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    int[] array = new int [LenghtArray];
+
+    for (int i = 0; i < LenghtArray; i++) {array[i] = new Random().Next(100, 1000);}
+
+    var str_array = string.Join(" ", array);
+    Console.WriteLine($"Создан массив:\n{str_array}");
+    return array;
+}
+
+int CountEvenNumbers(int[] array)
+// The method get count of even numbers
+
+{
+    int count = 0;
+
+    foreach (int value in array) {if (value % 2 == 0) count++;}
+    return count;
+}
+
+int[] UserArray = FillArray();
+int CountEvenNumbersUA = CountEvenNumbers(UserArray);
+Console.WriteLine($"Количество четных чисел в массиве {CountEvenNumbersUA}");
---
+++ Seminar_5.36/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+int[] FillArray()
+// The method creates an array of random numbers
+
+{
+    Console.WriteLine("Введите длину массива:");
+    int LenghtArray = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    int[] array = new int [LenghtArray];
+
+    for (int i = 0; i < LenghtArray; i++) {array[i] = new Random().Next(1, 1000);}
+
+    var str_array = string.Join(" ", array);
+    Console.WriteLine($"Создан массив:\n{str_array}");
+    return array;
+}
+
+int SumOddNumbers(int[] array)
+// The method finds the sum of elements standing in odd positions
+
+{
+    int SumNumbers = 0;
+
+    for (int i = 0; i < array.Length; i++) {if (i % 2 == 1) SumNumbers += array[i];}
+    return SumNumbers;
+}
+
+int[] UserArray = FillArray();
+int SumNumbersUA = SumOddNumbers(UserArray);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях равна {SumNumbersUA}");
+
+
