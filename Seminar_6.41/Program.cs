478f11ffa14bdd816fdbe2049c3c45a20810cdb5
Ell161 <ev.chepiga.it@yandex.ru>
2023-01-22 12:50:57 +0300

    Seminar 6 homework

---
+++ Seminar_6.41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int CountPositiveNumbers(int LenghtList)
+// The method counts the number of numbers greater than zero in the list
+
+{
+    int[] numbers = new int[LenghtList];
+    int CountPNumbers = 0;
+    for (int i = 0; i < LenghtList; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        try {numbers[i] = Convert.ToInt32(Console.ReadLine());}
+        catch 
+        {
+            Console.Write("Некорректный ввод, введите целое число: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }        
+        if (numbers[i] > 0) CountPNumbers++;
+    }
+    var str_array = string.Join(" ", numbers);
+    Console.WriteLine($"Создан список чисел: {str_array}");
+    return CountPNumbers;
+}
+
+Console.Write("Введите длину списка: ");
+int LenghtUserList = Convert.ToInt32(Console.ReadLine());
+int CountPNumbersUserList = CountPositiveNumbers(LenghtUserList);
+Console.WriteLine($"Количество чисел больше нуля равно {CountPNumbersUserList}");
---
+++ Seminar_6.43/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Dictionary<string, int> SetParametrsTwoLines()
+// The method sets parameters for two straight lines
+
+{
+    Dictionary<string, int> ParametrsLines = new Dictionary<string, int>()
+    {
+        { "k1", 0},
+        { "b1", 0},
+        { "k2", 0},
+        { "b2", 0},
+    };
+
+    foreach (var (key, value) in ParametrsLines)
+    {
+        Console.Write($"Задайте параметр {key}: ");
+        try {ParametrsLines[key] = Convert.ToInt32(Console.ReadLine());}
+        catch
+        {
+            Console.Write("Некорректный ввод, введите целое число: ");
+            ParametrsLines[key] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    Console.WriteLine("Получены следующие значения:");
+    foreach (var (key, value) in ParametrsLines) {Console.WriteLine($"{key} - {value}");}
+    return ParametrsLines;
+}
+
+void DefineIntersectionPoint(Dictionary<string, int> parametrs)
+// The method determines the intersection point of two straight lines
+{
+    if ((parametrs["k1"] == parametrs["k2"]) && (parametrs["b1"] == parametrs["b2"])) Console.WriteLine("Прямые совпадают");
+    else if (parametrs["k1"] == parametrs["k2"]) Console.WriteLine("Прямые параллельны");
+    else
+    {
+        var PointX = -(parametrs["b1"] - parametrs["b2"]) / (parametrs["k1"] - parametrs["k2"]);
+        var PointY = parametrs["k1"] * PointX + parametrs["b1"];
+        Console.WriteLine($"Прямые пересекаются в точке со значениями х - {PointX}, y - {PointY}");
+    }
+}
+
+Dictionary<string, int> UserParametrs = SetParametrsTwoLines();
+DefineIntersectionPoint(UserParametrs);
